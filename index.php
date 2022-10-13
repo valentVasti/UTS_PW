e42dc1278c09e559d8c186c3aa3222ff24ff483a
valentVasti <valentvasti2114@gmail.com>
2022-10-13 22:30:46 +0700

    landing page edit

--- index.php
+++ index.php
@@ -6,44 +6,45 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Bootstrap CSS -->
         <link
-href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-rel="stylesheet" integrity="sha384-
-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-crossorigin="anonymous">
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet" integrity="sha384-
+            EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossorigin="anonymous">
         <link href="style.css" rel="stylesheet">
-        <title>Pemrograman Web - PHP</title>
+        <title>E-Library</title>
     </head>
     <body>
-        <nav class="navbar navbar-dark bg-dark fixed-top">
+        <nav class="navbar navbar-light fixed-top">
             <div class="container">
                 <!-- Nama : nama panggilan kalian -->
-                <a class="navbar-brand fw-bold" href="/gd4_d_10947">PHP</a>
+                <a class="navbar-brand fw-bold" href="/gd4_d_10947"><img src="../asset/logo.png" alt="logo" style="width:35px"> E-Library</a>
+                
                 <div class="d-flex justify-content-end">
-                    <button class="btn btn-primary me-2" type="button" >
+                    <button class="btn me-2" type="button" >
                         <a class="text-light" style="text-decoration: none"
-href="./page/registerPage.php">Sign Up</a>
+                        href="./page/registerPage.php">Sign Up</a>
                     </button>
-                    <button class="btn btn-warning ms-2" type="button"
-href="./loginPage.php">
-                    <a class="text-light" style="text-decoration: none"
-href="./page/loginPage.php">Login</a>
+                    <button class="btn loginBtn ms-2" type="button" >
+                        <a class="text-light" style="text-decoration: none"
+                        href="./page/loginPage.php">Login</a>
                     </button>
                 </div>
             </div>
         </nav>
         <div class="bg">
-        <div class="container min-vh-100 d-flex align-items-center">
-            <div class="pt-5 mt-4">
-                <h2 class="text-center align-middle text-white"><b class="bg-dark">
-Welcome Perpus Atma</b></h2>
-            </div>
+            <div class="container min-vh-100 d-flex justify-content-center">
+                <div class="pt-5 mt-4">
+                    <h2 class="text-center align-middle text-black landingTitle">
+                        Welcome To E-Library</h2>
+                    <p class="text-center align-middle"><i>"Books are a uniquely portable magic." - Stephen King</i></p>
+                </div>
             </div>
         </div>
 <!-- Option 1: Bootstrap Bundle with Popper -->
         <script
-src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-crossorigin="anonymous">
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous">
         </script>
     </body>
 </html>
